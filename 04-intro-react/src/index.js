bdf46d955ee83892330d4a53af93e4111b01c31b
Branimir Espinoza <brani.espinoza@gmail.com>
2022-02-23 22:36:43 -0400

    Fetch api and custom hooks module finished

--- 04-intro-react/src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDom from 'react-dom';
-import React from 'react';
-import App from './App';
-import './index.css';
-
-const root = document.getElementById('root');
-ReactDom.render(<React.StrictMode><App /></React.StrictMode>, root);
