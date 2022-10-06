7fc24548464defbc0299e1c5249e1f084c9a680a
Branimir Espinoza <brani.espinoza@gmail.com>
2022-10-05 23:33:46 -0400

    Finishing section 6 react course with Vite

---
+++ 04-gif-expert-app-vite/src/components/AddCategory.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+export const AddCategory = ({onAddCategory}) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const onInputChange = ({target}) => {
+    setInputValue(target.value);
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const value = inputValue.trim();
+    if (value.length <= 1) return;
+    onAddCategory(value);
+    setInputValue('');
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type='text'
+        placeholder='Search gifs'
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+    
+  )
+}
+
